a786471cfd3bbe92ed9ba70f1acfafe93d65286e
shady salem <shady.salem307@gmail.com>
2024-01-11 15:45:34 +0200

    Added the first two screens and controllers

---
+++ src/main/java/com/example/snakesandladders/Controller/InitializeGame.java
@@ -0,0 +1,11 @@
+package com.example.snakesandladders.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class InitializeGame {
+
+    @FXML
+    private Pane MainPane;
+
+}
--- src/main/java/com/example/snakesandladders/Controller/MainController.java
+++ src/main/java/com/example/snakesandladders/Controller/MainController.java
@@ -1,14 +1,36 @@
-package com.example.snakesandladders;
+package com.example.snakesandladders.Controller;
 
+import com.example.snakesandladders.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button GameHistoryBtn;
+
+    @FXML
+    private Button QuestionManagementBtn;
+
+    @FXML
+    private Button StartGameBtn;
+
+    public void StartGame() throws IOException {
+        Stage stage = (Stage) StartGameBtn.getScene().getWindow();
+        stage.close();
+
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/snakesandladders/InitializeGame.fxml"));
+        primaryStage.setTitle("Welcome!");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
     }
+
 }
